14f8c1430cb6b50d1c46a5eb051f305491801586
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-08-08 08:21:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
+
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import UsersPage from './components/UsersPage';
+import NotificationsPage from './components/NotificationsPage';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/users" component={UsersPage} />
+        <Route path="/notifications" component={NotificationsPage} />
+        <Route path="/create-post" component={CreatePost} />
+        <Route path="/edit-post/:id" component={EditPost} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
